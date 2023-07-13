be44f9841e19c5b9be01dbc04459fb34863693c2
Ezinwa charles <charleskelvin12@gmail.com>
2023-07-13 09:02:09 +0100

    like

---
+++ backend/app/models/like.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from app import db
+
+class Like(db.Model):
+    __tablename__ = 'likes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, user_id, post_id):
+        self.user_id = user_id
+        self.post_id = post_id
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
