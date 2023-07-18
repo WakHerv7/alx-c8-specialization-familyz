709b2782c85ef06520a56efeb1a4a0c2211d0b6e
WakHerv7 <wakam.hermann@gmail.com>
2023-07-18 09:33:23 +0100

    updates-frontend-20230718-003

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,32 @@
-
-import Welcome from "./pages/welcome/Welcome";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Listpage from "./pages/listpage/Listpage";
+import Profilepage from "./pages/profile-page/Profilepage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Myfamilies from './pages/myfamilies/Myfamilies';
+import FamilyTree from "./pages/family-tree/FamilyTree.js";
 import Home from "./pages/home/Home";
-
-
-
-const isLogged = true;
 
 function App() {
   return (
-    <div className="App">
-      {isLogged ? <Home /> : <Welcome />}
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/login" element={<Login />}/>
+          <Route exact path="/signup" element={<Signup />}/>
+          <Route exact path="/listpage" element={<Listpage />}/>
+          <Route exact path="/profilepage" element={<Profilepage />}/>
+          <Route exact path="/myfamilies" element={<Myfamilies />}/>
+          <Route exact path="/family-tree" element={<FamilyTree />}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,56 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import './index.css';
+import App from './App';
 import {Store} from './store/Store.js';
 import {Provider} from 'react-redux';
-import App from './App';
-import Login from "./pages/login/Login";
-import Signup from "./pages/signup/Signup";
-import Listpage from "./pages/listpage/Listpage";
-import Profilepage from "./pages/profile-page/Profilepage";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Myfamilies from './pages/myfamilies/Myfamilies';
-import FamilyTree from "./pages/family-tree/FamilyTree.js";
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/listpage",
-    element: <Listpage />,
-  },
-  {
-    path: "/profilepage",
-    element: <Profilepage />,
-  },
-  {
-    path: "/myfamilies",
-    element: <Myfamilies />,
-  },
-  {
-    path: "/family-tree",
-    element: <FamilyTree />,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={Store}>
-      <RouterProvider router={router} />
+      <App />
     </Provider>
   </React.StrictMode>
 );
