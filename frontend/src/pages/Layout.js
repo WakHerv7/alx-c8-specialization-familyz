08d28da387c4f00277ff95ce142789bb4f52aad6
WakHerv7 <wakam.hermann@gmail.com>
2023-07-20 12:25:05 +0100

    update-link

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 import Header from '../components/Header';
 
 function Layout(props) {
-    const [sidebarOpen, setSidebarOpen] = useState(false);
     return (
         <div>
             <Header/>
